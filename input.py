f92770f517fe7f48592226d2dfcce2b09e931570
Sadaqat Ali Shaker <sadaqatalishakir786@gmail.com>
2025-04-11 01:27:18 +0500

    Improved code for age calculation in input.py

--- input.py
+++ input.py
@@ -1,4 +1,8 @@
-c= int(input("Enter ur date"))
+# Asking for the user's birth year
+birth_year = int(input("Enter your birth year: "))
 
-a=2025-c
-print(a)
+# Calculating the age based on 2025
+age = 2025 - birth_year
+
+# Printing the result
+print(f"Your age in 2025 will be: {age}")
